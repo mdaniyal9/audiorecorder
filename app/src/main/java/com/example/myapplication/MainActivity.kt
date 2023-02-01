8721cdfc1325fd168ebe42e5df75307c9a2d41da
daniyal <daniyal@cognitivehealthintl.com>
2023-02-01 17:14:22 +0500

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,183 @@
+package com.example.myapplication
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Camera
+import android.media.*
+import android.media.audiofx.AcousticEchoCanceler
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG: String = "MainActivity"
+
+    lateinit var audioManager: AudioManager
+
+    var isRecording = false
+    var record: AudioRecord? = null
+    var track: AudioTrack? = null
+
+    val RC_ALL_PERMISSIONS = 9005
+    private val mPermissionsNotGranted: MutableList<String> = java.util.ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        allPermissionGranted()
+        startHardwareTest()
+    }
+
+    private fun startHardwareTest(){
+
+        audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+        initRecordAndTrack()
+
+        audioManager.isSpeakerphoneOn = true
+
+        object : Thread() {
+            override fun run() {
+                recordAndPlay()
+            }
+        }.start()
+        startRecordAndPlay()
+    }
+
+    private fun initRecordAndTrack() {
+        val min = AudioRecord.getMinBufferSize(
+            AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_MUSIC),
+            AudioFormat.CHANNEL_IN_MONO,
+            AudioFormat.ENCODING_PCM_16BIT
+        )
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        record = AudioRecord(
+            MediaRecorder.AudioSource.MIC,
+            AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_MUSIC),
+            AudioFormat.CHANNEL_IN_STEREO,
+            AudioFormat.ENCODING_PCM_16BIT,
+            min
+        )
+        if (AcousticEchoCanceler.isAvailable()) {
+            val echoCancler = AcousticEchoCanceler.create(record!!.getAudioSessionId())
+            echoCancler.enabled = true
+        }
+//        val maxJitter = AudioTrack.getMinBufferSize(
+//            8000,
+//            AudioFormat.CHANNEL_OUT_MONO,
+//            AudioFormat.ENCODING_PCM_16BIT
+//        )
+        track = AudioTrack(
+            AudioManager.STREAM_MUSIC,
+            8000,
+            AudioFormat.CHANNEL_IN_STEREO,
+            AudioFormat.ENCODING_PCM_16BIT,
+            min,
+            AudioTrack.MODE_STREAM
+        )
+        track!!.playbackRate = AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_MUSIC)
+
+    }
+
+    private fun recordAndPlay() {
+        val min = AudioRecord.getMinBufferSize(
+            AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_MUSIC),
+            AudioFormat.CHANNEL_IN_MONO,
+            AudioFormat.ENCODING_PCM_16BIT
+        )
+        val lin = ShortArray(min)
+        var num: Int? = 0
+        audioManager.setMode(AudioManager.MODE_NORMAL)
+        while (true) {
+            if (isRecording) {
+//                num = (record?.read(lin, 0, lin.size) ?: track?.write(lin, 0, num!!))
+                record?.read(lin, 0, lin.size)
+
+                for (i in lin.indices) {
+                    lin[i] = (lin[i].toInt() * 1).coerceAtMost(Int.MAX_VALUE).toShort()
+                }
+                track?.write(lin, 0, lin.size)
+            }
+        }
+    }
+
+    private fun startRecordAndPlay() {
+        record?.startRecording()
+        track?.play()
+        isRecording = true
+    }
+
+    private fun stopRecordAndPlay() {
+        record?.stop()
+        track?.pause()
+        isRecording = false
+    }
+
+    private fun allPermissionGranted(): Boolean
+    {
+        mPermissionsNotGranted.clear()
+        micCheckGranted()
+
+        if(mPermissionsNotGranted.size == 0)
+        {
+            return true
+        }
+
+        ActivityCompat.requestPermissions(
+            this,
+            mPermissionsNotGranted.toTypedArray(), RC_ALL_PERMISSIONS)
+
+        return false
+    }
+
+    private fun micCheckGranted()
+    {
+        val res = ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+        if (res == PackageManager.PERMISSION_GRANTED)
+            return
+
+        mPermissionsNotGranted.add(Manifest.permission.RECORD_AUDIO)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.e(TAG, "onRequestPermissionsResult: ${permissions.contentToString()}++++++++++++${grantResults.contentToString()}")
+
+        var deniedAny = false
+
+        if(requestCode == RC_ALL_PERMISSIONS)
+        {
+            val j = 0
+            for(i in grantResults)
+            {
+                Log.e(TAG, "onRequestPermissionsResult: $i")
+
+                if(i != PackageManager.PERMISSION_GRANTED)
+                    deniedAny = true
+            }
+
+            if(deniedAny)
+            {
+                Log.e(TAG, "onRequestPermissionsResult: Permission Denied")
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        stopRecordAndPlay()
+        super.onDestroy()
+    }
+
+}
